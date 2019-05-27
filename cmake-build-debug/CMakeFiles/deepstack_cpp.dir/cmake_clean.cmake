9c93e3538db09361c54acef475c20c07219d99a0
zhou <zhoujz10@163.com>
2019-05-27 12:32:08 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/deepstack_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deepstack_cpp.dir/main.cpp.o"
+  "deepstack_cpp.pdb"
+  "deepstack_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deepstack_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ terminal_equity.h
@@ -0,0 +1,17 @@
+//
+// Created by zhou on 2019-05-27.
+//
+
+#ifndef DEEPSTACK_CPP_TERMINAL_EQUITY_H
+#define DEEPSTACK_CPP_TERMINAL_EQUITY_H
+
+class TerminalEquity {
+
+    TerminalEquity() {
+
+    };
+    ~TerminalEquity();
+};
+
+
+#endif //DEEPSTACK_CPP_TERMINAL_EQUITY_H
